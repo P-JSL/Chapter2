e2cf058e929730fb7cc7c2112258b2f81f55ab42
P-JSL <althsuwpfl@gmail.com>
2023-08-13 21:02:53 +0900

    2장 끝

---
+++ Arguments/Program.cs
@@ -0,0 +1,33 @@
+﻿#define MYSIMBOL
+using static System.Console;
+
+foreach (string arg in args)
+{
+  WriteLine(arg);
+}
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of cursor");
+}
+
+if (OperatingSystem.IsWindows())
+{
+    //윈도우에서만 실행되는 코드
+}
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    //윈도우 10에서만 실행 되는 코드
+}
+else if (OperatingSystem.IsIOSVersionAtLeast(major:14, minor:5))
+{
+    //IOS 14.5 이상에서 작동 되는 코드
+}
+else if (OperatingSystem.IsBrowser())
+{
+    //블레이저를 사용하는 브라우저에서만 코드 실행
+}
---
+++ Exercise03/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Type\tByte(s) of memory\tMin\t\t\t\tMax");
+Console.WriteLine("------------------------------------------------------------------------------------");
+Console.WriteLine($"sbyte\t{sizeof(sbyte)}\t\t\t{sbyte.MinValue:N0}\t\t\t\t{sbyte.MaxValue:N0}");
+Console.WriteLine($"byte\t{sizeof(byte)}\t\t\t{byte.MinValue:N0}\t\t\t\t{byte.MaxValue:N0}");
+Console.WriteLine($"short\t{sizeof(short)}\t\t\t{short.MinValue:N0}\t\t\t\t{short.MaxValue:N0}");
+Console.WriteLine($"ushort\t{sizeof(ushort)}\t\t\t{ushort.MinValue:N0}\t\t\t\t{ushort.MaxValue:N0}");
+Console.WriteLine($"int\t{sizeof(int)}\t\t\t{int.MinValue:N0}\t\t\t{int.MaxValue:N0}");
+Console.WriteLine($"uint\t{sizeof(uint)}\t\t\t{uint.MinValue:N0}\t\t\t\t{uint.MaxValue:N0}");
+Console.WriteLine($"long\t{sizeof(long)}\t\t\t{long.MinValue:N0}\t{long.MaxValue:N0}");
+Console.WriteLine($"ulong\t{sizeof(ulong)}\t\t\t{ulong.MinValue:N0}\t\t\t\t{ulong.MaxValue:N0}");
+Console.WriteLine($"float\t{sizeof(float)}\t\t\t{float.MinValue:N0}\t\t\t\t{float.MaxValue:N0}");
+Console.WriteLine($"double\t{sizeof(double)}\t\t\t{double.MinValue:N0}\t\t\t\t{double.MaxValue:N0}");
+Console.WriteLine($"decimal\t{sizeof(decimal)}\t\t\t{decimal.MinValue:N0}\t\t\t\t{decimal.MaxValue:N0}");
